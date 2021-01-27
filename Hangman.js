c484c6a545ca6470f397ce2d589aab672eda971c
Khalil Abdellah <kabdellah@Khalils-MacBook-Air.local>
2021-01-27 14:23:19 -0500

    a

--- Hangman.js
+++ Hangman.js
@@ -1,14 +1,50 @@
+let displayMessage;
+let guessHappened = false;
 
 class Hangman {
 
-    getDisplayMessage(){
-        return null;
+    constructor(phrase){
+        this.phrase = phrase;
+        this.guesses = 6;
+        this.message = [];
+        this.lettersGuess = [];
     }
 
-    guessLetter(letter){}
+    getDisplayMessage(){
+        if (guessHappened) {
+            const regex = /\S/g;
+            displayMessage = this.phrase.replace(regex, '*');
+        }
+
+        return displayMessage;
+    }
+
+    guessLetter(letter){
+        var count = 0;
+        guessHappened = true;
+
+        const regex = /^\s|^letter/i;
+
+        for (var i = 0 ; i < this.phrase.length ; i++ ) {
+            if (letter.toLowerCase() == this.phrase[i].toLowerCase()) {
+                count++
+            } 
+        }
+            if (count == 0) {
+                this.guesses--;
+            } else {
+                for (let i = 0 ; i < this.phrase ; i++) {
+                    displayMessage = this.phrase.replace(regex, '*');
+                    
+                }   
+
+            }
+    }
+    // loop thru phrase. phrase is turned into ** in displaymessage but not globally
+    // to make it global
 
     getRemainingGuesses(){
-        return -1;
+        return this.guesses;
     }
 
     isMessageSolved(){
